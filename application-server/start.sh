425ab391e9b1a7fe16bd0a8fb8ea95b51ba38ce5
Jason Stock <stock@columbia.cs.colostate.edu>
2019-11-17 14:17:35 -0700

    added ability to start-all without compiling

--- application-server/start.sh
+++ application-server/start.sh
@@ -17,6 +17,17 @@ JAR_PATH="$DIR/conf/:$DIR/build/libs/application-server-1.0-SNAPSHOT.jar"
 MACHINE_LIST="$DIR/conf/machine_list"
 APPLICATION_PROPERTIES="$DIR/conf/application.properties"
 
+function usage {
+cat << EOF
+    
+    script usage: $( basename $0 ) [-o operation]
+
+    -o operation   : 'execute' or 'compile' and exit
+    
+EOF
+    exit 1
+}
+
 function prop {
     grep "${1}" ${APPLICATION_PROPERTIES}|cut -d'=' -f2
 }
@@ -29,15 +40,45 @@ function jumpto
     exit
 }
 
+NUM_CLIENTS=1
+OPERATION="compile"
+EXIT=0
+
+while getopts o: option
+do
+    case "${option}" in
+        o) OPERATION=${OPTARG}
+        if [ "$OPERATION" = "compile" ]
+        then
+            EXIT=1
+        fi
+        ;;
+        ?) usage;;
+    esac
+done
+
+# Compile
+
+if [ "$OPERATION" = "compile" ]
+then
+    
+    LINES=`find . -name "*.java" -print | xargs wc -l | grep "total" | awk '{$1=$1};1'`
+    echo Project has "$LINES" lines
+
+    gradle clean; gradle build
+    
+    if [ $EXIT == 1 ]
+    then
+        exit
+    fi
+fi
+
 # Launch Switch
 
-LINES=`find . -name "*.java" -print | xargs wc -l | grep "total" | awk '{$1=$1};1'`
-echo Project has "$LINES" lines
-
-gradle clean; gradle build
 gnome-terminal --geometry=170x50 -t "Switch" -e "ssh -t $(prop 'switch.host') cd '$DIR'; 'java -cp $JAR_PATH distributed.application.node.Switch; bash;'"
 
 # Launch servers
+
 sleep 3
 server:
 
